6173d3c602d9a7d85cf2941a634b60636999737e
Adnane <thehazardousway@live.Fr>
2023-03-30 02:35:05 +0200

    'V3'

--- src/content/bio.js
+++ src/content/bio.js
@@ -5,6 +5,6 @@ export default {
     logoLightUrl: '',
     logoDarkUrl: '',
     tagline: 'Analyste Developpeur', // e.g Building Product Experiences
-    photo1: '../public/Me.jpeg?cs=tinysrgb&h=750&w=1260',
+    photo1: '/Me.jpeg?cs=tinysrgb&h=750&w=1260',
     //    photo1: 'https://images.pexels.com/photos/7907687/pexels-photo-7907687.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
 }
